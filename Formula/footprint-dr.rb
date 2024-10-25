085deffd9078caf27379402c6a1b8db7295244a8
Ethan Lowman <155459438+ethan-lowman-fp@users.noreply.github.com>
2024-10-25 17:33:58 -0400

    Add footprint-dr 0.3.0 (#3)

--- Formula/footprint-dr.rb
+++ Formula/footprint-dr.rb
@@ -2,11 +2,11 @@ class FootprintDr < Formula
   desc "Footprint Disaster Recovery CLI"
   homepage "https://docs.onefootprint.com/"
 
-  version = "0.2.0"
+  version = "0.3.0"
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/onefootprint/footprint-dr-releases/releases/download/#{version}/footprint-dr-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "1783cdef3716aa6a37cb07b020431a8dbe8629a1ac713c86c3cdfa75163f491a"
+      sha256 "f4ecce2fe7b0c3a6582beefcd823d98025277a5a63d7f29218a3426063878798"
 
       def install
         bin.install "footprint-dr"
